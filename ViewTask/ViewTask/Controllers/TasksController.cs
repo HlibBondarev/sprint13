8314bd7e47c025c7c75922387fd0ef544e8deaa1
HlibBondarev <Hlib.Bondarev@gmail.com>
2023-12-01 14:23:08 +0200

    Added List<Product> into TaskController and file .gitignore

--- ViewTask/ViewTask/Controllers/TasksController.cs
+++ ViewTask/ViewTask/Controllers/TasksController.cs
@@ -1,40 +1,63 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using ViewTask.Models;
 
 namespace ViewTask.Controllers
 {
-    public class TasksController : Controller
-    {
+	public class TasksController : Controller
+	{
+		readonly List<Product> products = new()
+		{
+			new Product {Name="Bread",Price=10 },
+			new Product {Name="Milk",Price=10 },
+			new Product {Name="Chease",Price=10 },
+			new Product {Name="Sausage",Price=10 },
+			new Product {Name="Potato",Price=10 },
+			new Product {Name="Banano",Price=10 },
+			new Product {Name="Tomato",Price=10 },
+			new Product {Name="Candy",Price=10 }
+		};
+
+		public TasksController(IEnumerable<Product> products)
+		{
+			this.products = (List<Product>)products;
+
+		}
+        public TasksController()
+        {
+        }
+
         public IActionResult Index()
-        {
-            return View();
-        }
-        public IActionResult SprintTasks()
-        {
-            return View();
-        }
-        public IActionResult Greetings()
-        {
-            return View();
-        }
-        public IActionResult ProductInfo()
-        {
-            return View();
-        }
-        public IActionResult ShoppingCart()
-        {
-            return View();
-        }
-        public IActionResult ShoppingList()
-        {
-            return View();
-        }
-        public IActionResult SuperMarkets()
-        {
-            return View();
-        }
-        //public IActionResult TimeToBuy()
-        //{
-        //    return View();
-        //}
-    }
+		{
+			return View();
+		}
+		public IActionResult SprintTasks()
+		{
+			return View();
+		}
+		public IActionResult Greetings()
+		{
+			return View();
+		}
+		public IActionResult ProductInfo()
+		{
+			return View();
+		}
+		public IActionResult ShoppingCart()
+		{
+			return View();
+		}
+		public IActionResult ShoppingList()
+		{
+			return View();
+		}
+		public IActionResult SuperMarkets()
+		{
+			return View();
+		}
+		//public IActionResult TimeToBuy()
+		//{
+		//    return View();
+		//}
+	}
 }
